4e5affa5a3e181e402b2f9bf37feccbf0699a14d
sauceaupoivre <sauceaupoivre99@gmail.com>
2022-12-17 01:31:52 +0100

    silent hill siren

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
-$(document).ready(function(){
+const audio = new Audio('assets/audio/siren.m4a');
 
+$(document).ready(function(){
+    
     setInterval(function() {
         var url = "http://localhost/InfoExam_Back/public/api/"+1;
         $.ajax({
@@ -13,13 +15,17 @@ $(document).ready(function(){
             {
                 if(response.alertes != null)
                 {
-                    alert("prout");
+                    audio.play();
+                    //$('#alerte-modal').modal('show');
+                    if (window.confirm("ALERLERLLERR")) {
+                        audio.pause();
+                    }
                 }
                 
             },
             error: function(response) {
             }
         });
-      }, 60000);
+      }, 10000);
 
 });
